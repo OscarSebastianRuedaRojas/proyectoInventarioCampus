621edac2425178c9f3f3e1894688f46591c1bc96
Oviel Felipe Mendoza Pineda <127790713+OvielFelipeMendozaPineda@users.noreply.github.com>
2024-03-12 06:43:46 -0500

    fix: :bug: Prueba 3
    Prueba 3

--- js/app.js
+++ js/app.js
@@ -1,24 +1,60 @@
-import '/blob/main/App/components/sidebar/sidebar.js';
-import '/blob/main/App/components/Forms/Activo/form.js'
-import '/blob/main/App/components/Forms/Estado/Estado.js'
-import '/blob/main/App/components/Forms/Marca/Marca.js'
-import '/blob/main/App/components/Forms/TipoPersona/TipoPersona.js'
-import '/blob/main/App/components/Alert/Alert.js'
-import '/blob/main/App/components/Forms/Persona/Persona.js'
-import '/blob/main/App/components/Forms/TipoActivo/TipoActivo.js'
-import '/blob/main/App/components/Delete/delete.js'
-import '/blob/main/App/components/DeleteActivo/deleteActivo.js'
-import '/blob/main/App/components/Home/Home.js'
-import '/blob/main/App/components/Editar/editarEstado/editarEstado.js'
-import '/blob/main/App/components/Editar/editarMarca/editarMarca.js'
-import '/blob/main/App/components/Editar/editarMovimientoActivo/editarMovimiento.js'
-import '/blob/main/App/components/Editar/editarPersona/editarPersona.js'
-import '/blob/main/App/components/Editar/editarTipoPersona/editarTipoPersona.js'
-import '/blob/main/App/components/Editar/editarTipoActivo/editarTipoActivo.js'
-import '/blob/main/App/components/Buscar/Buscar.js'
-import '/blob/main/App/components/Editar/editarActivo/editarActivo.js'
-import '/blob/main/App/components/Ubicacion/ubicacion.js'
-import '/blob/main/App/components/mantenimiento/mantenimiento.js'
-import '/blob/main/App/components/bajas/bajas.js'
-import '/blob/main/App/components/crearPortafolio/portafolio.js'
-import '/blob/main/App/components/asignacion/asignar.js'
+import 'https://raw.githubusercontent.com/oscarsebastianruedarojas/NombreDeTuRepositorio/main/App/components/sidebar/sidebar.js';
+import 'https://raw.githubusercontent.com/oscarsebastianruedarojas/NombreDeTuRepositorio/main/App/components/Forms/Activo/form.js';
+import 'https://raw.githubusercontent.com/oscarsebastianruedarojas/NombreDeTuRepositorio/main/App/components/Forms/Estado/Estado.js';
+import 'https://raw.githubusercontent.com/oscarsebastianruedarojas/NombreDeTuRepositorio/main/App/components/Forms/Marca/Marca.js';
+import 'https://raw.githubusercontent.com/oscarsebastianruedarojas/NombreDeTuRepositorio/main/App/components/Forms/TipoPersona/TipoPersona.js';
+import 'https://raw.githubusercontent.com/oscarsebastianruedarojas/NombreDeTuRepositorio/main/App/components/Alert/Alert.js';
+import 'https://raw.githubusercontent.com/oscarsebastianruedarojas/NombreDeTuRepositorio/main/App/components/Forms/Persona/Persona.js';
+import 'https://raw.githubusercontent.com/oscarsebastianruedarojas/NombreDeTuRepositorio/main/App/components/Forms/TipoActivo/TipoActivo.js';
+import 'https://raw.githubusercontent.com/oscarsebastianruedarojas/NombreDeTuRepositorio/main/App/components/Delete/delete.js';
+import 'https://raw.githubusercontent.com/oscarsebastianruedarojas/NombreDeTuRepositorio/main/App/components/DeleteActivo/deleteActivo.js';
+import 'https://raw.githubusercontent.com/oscarsebastianruedarojas/NombreDeTuRepositorio/main/App/components/Home/Home.js';
+import 'https://raw.githubusercontent.com/oscarsebastianruedarojas/NombreDeTuRepositorio/main/App/components/Editar/editarEstado/editarEstado.js';
+import 'https://raw.githubusercontent.com/oscarsebastianruedarojas/NombreDeTuRepositorio/main/App/components/Editar/editarMarca/editarMarca.js';
+import 'https://raw.githubusercontent.com/oscarsebastianruedarojas/NombreDeTuRepositorio/main/App/components/Editar/editarMovimientoActivo/editarMovimiento.js';
+import 'https://raw.githubusercontent.com/oscarsebastianruedarojas/NombreDeTuRepositorio/main/App/components/Editar/editarPersona/editarPersona.js';
+import 'https://raw.githubusercontent.com/oscarsebastianruedarojas/NombreDeTuRepositorio/main/App/components/Editar/editarTipoPersona/editarTipoPersona.js';
+import 'https://raw.githubusercontent.com/oscarsebastianruedarojas/NombreDeTuRepositorio/main/App/components/Editar/editarTipoActivo/editarTipoActivo.js';
+import 'https://raw.githubusercontent.com/oscarsebastianruedarojas/NombreDeTuRepositorio/main/App/components/Buscar/Buscar.js';
+import 'https://raw.githubusercontent.com/oscarsebastianruedarojas/NombreDeTuRepositorio/main/App/components/Editar/editarActivo/editarActivo.js';
+import 'https://raw.githubusercontent.com/oscarsebastianruedarojas/NombreDeTuRepositorio/main/App/components/Ubicacion/ubicacion.js';
+import 'https://raw.githubusercontent.com/oscarsebastianruedarojas/NombreDeTuRepositorio/main/App/components/mantenimiento/mantenimiento.js';
+import 'https://raw.githubusercontent.com/oscarsebastianruedarojas/NombreDeTuRepositorio/main/App/components/bajas/bajas.js';
+import 'https://raw.githubusercontent.com/oscarsebastianruedarojas/NombreDeTuRepositorio/main/App/components/crearPortafolio/portafolio.js';
+import 'https://raw.githubusercontent.com/oscarsebastianruedarojas/NombreDeTuRepositorio/main/App/components/asignacion/asignar.js';
+
+
+
+
+
+
+
+
+
+
+
+
+// import '/blob/main/App/components/sidebar/sidebar.js';
+// import '/blob/main/App/components/Forms/Activo/form.js'
+// import '/blob/main/App/components/Forms/Estado/Estado.js'
+// import '/blob/main/App/components/Forms/Marca/Marca.js'
+// import '/blob/main/App/components/Forms/TipoPersona/TipoPersona.js'
+// import '/blob/main/App/components/Alert/Alert.js'
+// import '/blob/main/App/components/Forms/Persona/Persona.js'
+// import '/blob/main/App/components/Forms/TipoActivo/TipoActivo.js'
+// import '/blob/main/App/components/Delete/delete.js'
+// import '/blob/main/App/components/DeleteActivo/deleteActivo.js'
+// import '/blob/main/App/components/Home/Home.js'
+// import '/blob/main/App/components/Editar/editarEstado/editarEstado.js'
+// import '/blob/main/App/components/Editar/editarMarca/editarMarca.js'
+// import '/blob/main/App/components/Editar/editarMovimientoActivo/editarMovimiento.js'
+// import '/blob/main/App/components/Editar/editarPersona/editarPersona.js'
+// import '/blob/main/App/components/Editar/editarTipoPersona/editarTipoPersona.js'
+// import '/blob/main/App/components/Editar/editarTipoActivo/editarTipoActivo.js'
+// import '/blob/main/App/components/Buscar/Buscar.js'
+// import '/blob/main/App/components/Editar/editarActivo/editarActivo.js'
+// import '/blob/main/App/components/Ubicacion/ubicacion.js'
+// import '/blob/main/App/components/mantenimiento/mantenimiento.js'
+// import '/blob/main/App/components/bajas/bajas.js'
+// import '/blob/main/App/components/crearPortafolio/portafolio.js'
+// import '/blob/main/App/components/asignacion/asignar.js'
